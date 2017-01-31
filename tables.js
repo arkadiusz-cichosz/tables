56a892d0478d0e46d53a79b581378219b07406d8
Arkadiusz Cichosz <cichorek87220@gmail.com>
2017-01-31 18:31:24 +0100

    add file from ex. 8.7

---
+++ tables.js
@@ -0,0 +1,8 @@
+var male=[Robert, Stanisław, Roman, Arkadiusz, Włodzimierz];
+var female=[Danuta, Jadwiga, Bożena,Kasia,Asia];
+var allNames=male.concat(female);
+var newName='Marian';
+
+if (allNames.indexOf(newName)===-1) {
+	allNames.push(newName);
+}

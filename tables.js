8f70711be54c6194ca0f84ece135984aaab28ee4
Arkadiusz Cichosz <cichorek87220@gmail.com>
2017-02-02 17:20:18 +0100

    Update tables.js
    add content view of array, fixed errors

--- tables.js
+++ tables.js
@@ -1,5 +1,5 @@
-var male = [Robert, Stanisław, Roman, Arkadiusz, Włodzimierz];
-var female = [Danuta, Jadwiga, Bożena,Kasia,Asia];
+var male = ['Robert', 'Stanisław', 'Roman', 'Arkadiusz', 'Włodzimierz'];
+var female = ['Danuta', 'Jadwiga', 'Bożena','Kasia','Asia'];
 var allNames = male.concat(female);
 var newName = 'Marian';
 
@@ -7,6 +7,9 @@ if (allNames.indexOf(newName) === -1) {
 	allNames.push(newName);
 }
 
-for (var i = 0; i < allNames.lenght(); i++) {
+console.log('Zawarość tablicy po zastosowaniu metody concat: ');
+
+for (var i = 0; i < allNames.length; i++) {
 	console.log(allNames[i]);
 }
+
